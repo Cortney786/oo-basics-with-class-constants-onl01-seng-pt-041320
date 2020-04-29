0d5849f100d73f6b4922dc5738242cf846cf70f4
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-29 14:20:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :color, 
-  attr_reader :brand, :condition, :material, :size
+  attr_accessor :color, :condition, :material,
+  attr_reader :brand, :size
   
   BRANDS = []
 
